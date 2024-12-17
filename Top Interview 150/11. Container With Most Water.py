11a7b33e89d1dec9ab1dd61f7758514ebf39c59e
chen7647 <chen7647@umn.edu>
2024-12-16 22:55:25 -0600

    11 - couldn't solve

--- Top Interview 150/11. Container With Most Water.py
+++ Top Interview 150/11. Container With Most Water.py
@@ -22,3 +22,29 @@ class Solution(object):
                 back_index -= 1
             
         return max_water
+    
+# thought process
+# 100% definite
+# - have 2 pointers iterating from both ends of the array
+# - the both pointers move towards each other
+
+# 50% sure
+# - idk how they are going to move towards each other (which one should take the closer step to each other)
+# - idk which is more important factor? (height or width)?
+
+# goal: 
+# - get as much water as possible
+
+# 2 main factor for collecting largest amount of water
+# - height --> i think this
+# - width 
+
+# .
+# ..
+# ...
+# ..
+# .
+# .
+# how do you even compare width? if you move, you're moving one step closer to each other... --> so its height the more important factor?
+
+# if height[]
